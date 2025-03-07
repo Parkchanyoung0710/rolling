706dcb8d5fbcfa815dd071483f90ff3c93f609e2
parkchanyoung0710 <call4892@naver.com>
2025-03-07 15:15:56 +0900

    datetime 설정 및 테스트

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
+import React from "react";
+import { formatDate } from "./utils/datetime";
 function App() {
+  const isoDate = "2025-03-07T12:34:56Z"; //예시 시간 설정
+
   return (
     <>
       <h1>Rolling 시작!</h1>
+      <h2>{formatDate(isoDate)}</h2>
     </>
   );
 }
